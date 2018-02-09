49c4f77a9af7fbf79e7f2da38f86073d4ed349d3
Liam Campbell <liamdanger@users.noreply.github.com>
2018-02-09 09:32:29 -0800

    Add typicode!
    Typicode links to their Patreon from several of their packages, notably https://www.npmjs.com/package/husky

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const authors = {
   shama: 'https://www.patreon.com/shama',
   sindresorhus: 'https://www.patreon.com/sindresorhus',
   staltz: 'https://en.liberapay.com/andrestaltz/',
+  typicode: 'https://www.patreon.com/typicode',
   thlorenz: 'https://www.patreon.com/thlorenz',
   yyx990803: 'https://www.patreon.com/evanyou',
   juliangruber: 'https://www.patreon.com/juliangruber'
