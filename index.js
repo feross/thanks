3747a547afab4e97471269d217456483892046c5
LitoMore <litomore@gmail.com>
2018-08-13 20:18:52 +0800

    Add `litomore` as author

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ const authors = {
   kgryte: 'https://www.patreon.com/athan',
   korynunn: 'https://www.patreon.com/korynunn',
   limonte: 'https://www.patreon.com/limonte',
+  litomore: 'https://paypal.me/LitoMore',
   loghorn: 'https://paypal.me/Loghorn',
   mafintosh: 'https://www.patreon.com/mafintosh',
   maoberlehner: 'https://paypal.me/maoberlehner',
